c7033366c277366c676871f622d30f3226284832
dobschenkman-alt <dobschenkman@gmail.com>
2026-04-20 19:17:06 -0400

    Update server2.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const Datastore = require('nedb-promises');
 
 const app = express();
 app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 if (!fs.existsSync('./db')) fs.mkdirSync('./db');
 
 const db = {
