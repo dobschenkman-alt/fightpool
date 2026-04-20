bd6542de7633c2410123bb19dfce58c5c07d74de
dobschenkman-alt <dobschenkman@gmail.com>
2026-04-20 19:20:05 -0400

    Update server3.js

--- server.js
+++ server.js
@@ -348,3 +348,7 @@ app.use(express.static('public'));
   await seedDatabase();
   app.listen(process.env.PORT||3000,()=>console.log('FightPool 2026 → http://localhost:3000'));
 })();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT);
+});
